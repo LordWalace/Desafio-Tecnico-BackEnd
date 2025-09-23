f3f35f9912ae1d9055aca159e192799cb4d241b1
LordWalace <walaceestudante@gmail.com>
2025-09-22 22:11:32 -0300

    Modulos do BackEnd finalizados

--- app/db/database.py
+++ app/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://USUARIO:SENHA@localhost/NOME_DO_BANCO" ## Adicionar o meu novo usuario e senha....C:\Program Files\PostgreSQL\<versão>\data\postgresql.conf
+***REMOVED***
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
---
+++ app/schemas/administrador.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+class AdminCreate(BaseModel):
+    username: str
+    password: str
+
+class AdminResponse(BaseModel):
+    id: int
+    username: str
+
+    class Config:
+        orm_mode = True
