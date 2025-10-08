8f68aa31bd8a3477ba60f67bf0bbe1cd9d410963
Walace de Jesus Venas <119073069+LordWalace@users.noreply.github.com>
2025-10-07 21:34:18 -0300

    Update auth_service.py
    Remoção de algumas variaveis.

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -2,9 +2,7 @@ from datetime import datetime, timedelta, timezone
 from passlib.context import CryptContext
 from jose import JWTError, jwt
 
-SECRET_KEY = "admin123" 
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
